ff30d9a66b9058ab75b8e8edafce1edd28b2104c
Pavan Chandraval <pavancha@outlook.com>
2019-09-24 01:04:07 +0530

    changes to BodyParams to QueryParams

--- src/main/java/com/techprimers/security/jwtsecurity/controller/FindBank.java
+++ src/main/java/com/techprimers/security/jwtsecurity/controller/FindBank.java
@@ -6,12 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.techprimers.security.jwtsecurity.Dao.BranchService;
 import com.techprimers.security.jwtsecurity.model.BankDetail;
-import com.techprimers.security.jwtsecurity.model.InputData;
 
 @Controller
 public class FindBank {
@@ -20,9 +19,8 @@ public class FindBank {
 	
 	@GetMapping("/rest/findBank")
 	@ResponseBody
-	public BankDetail getBankData(@RequestBody final InputData inputData) {
+	public BankDetail getBankData(@RequestParam final String ifscCode) {
 		BranchService branchService = new BranchService(environment);
-		String ifscCode = inputData.getIfsc();
         if(ifscCode==null) {
         	return null;
         }
@@ -31,18 +29,11 @@ public class FindBank {
 	
 	@GetMapping("/rest/findAllBank")
 	@ResponseBody
-	public List<BankDetail> getAllBankData(@RequestBody final InputData inputData) {
+	public List<BankDetail> getAllBankData(@RequestParam final String bankName,@RequestParam final String city,@RequestParam final int limit,@RequestParam final int offset) {
 		BranchService branchService = new BranchService(environment);
-		String bankName  = inputData.getBankName();
-		String city = inputData.getCity();
-        if(bankName==null||city==null) {
+		 if(bankName==null||city==null) {
         	return null;
         }
-        int limit = inputData.getLimit();
-        int offset = inputData.getOffset();
         return branchService.findAll(bankName, city,limit,offset);
     }
-	
-	
-
 }
